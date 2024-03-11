d41edb0392db5954303d99f23d368f00d68138fa
guadasandoval <guadastoop@gmail.com>
2024-03-11 13:48:46 -0300

    mvp

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let emptyElement = document.querySelector('.about .empty')
 let titleElement = document.querySelector('.about .title')
 let figureElement = document.querySelectorAll('figure')
 let imgLogo = document.querySelectorAll('.logo')
+let formElement = document.getElementById('forma-desing')
 let index = 0
 
 window.addEventListener('mousemove', handleMouseMove)
@@ -25,3 +26,25 @@ function fadeNext() {
   }
   fadeNext();
   setInterval(fadeNext, 2000);
+
+  function crearFigura(event) {
+    const contenedor = document.getElementById('contenedor');
+    
+    const rect = contenedor.getBoundingClientRect();
+    const offsetX = event.clientX - rect.left;
+    const offsetY = event.clientY - rect.top;
+
+    const nuevaFigura = document.createElement('img');
+    nuevaFigura.src = './images/e.svg';
+    nuevaFigura.classList.add('figura');
+
+    const maxWidth = contenedor.clientWidth - nuevaFigura.offsetWidth;
+    const maxHeight = contenedor.clientHeight - nuevaFigura.offsetHeight;
+    const leftPosition = Math.min(Math.max(offsetX - nuevaFigura.offsetWidth / 2, 0), maxWidth);
+    const topPosition = Math.min(Math.max(offsetY - nuevaFigura.offsetHeight / 2, 0), maxHeight);
+    
+    nuevaFigura.style.left = leftPosition + 'px';
+    nuevaFigura.style.top = topPosition + 'px';
+
+    contenedor.appendChild(nuevaFigura);
+} 
